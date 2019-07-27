08252c85dae58ece3bdfbbd226facbe3cecfc915
Razelle McCarrick <razelle@satyaorthopedic.com>
2019-07-27 14:37:49 -0400

    styled and modified error message to reflect working on current issue

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="outer-container">
-    <div class="back">
-      <nuxt-link to="/">back to list</nuxt-link>
-    </div>
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <nuxt-link to="/" style="text-decoration: underline;">Return to home</nuxt-link>
+  <div class="page flex-center-column">
+    <h1 v-if="error.statusCode === 404">Page Not Found</h1>
+    <p class="padding-bottom-2" v-if="error.statusCode === 404">
+      * If you are receiving this error when trying to navigate to
+      <code>/blog</code> from within this site, please note that I am working to fix this issue. Temporarily, you can refresh the blog route and search the posts, but for now will not be able to access an individual post.
+    </p>
+    <h1 v-else>Oh no! An error occurred for some reason</h1>
+    <nuxt-link to="/" style="text-decoration: underline;">Return Home</nuxt-link>
   </div>
 </template>
 <script>
@@ -16,9 +17,6 @@ export default {
     return {
       title: "Prismic Nuxt.js Blog"
     };
-  },
-  mounted() {
-    this.$router.go(1);
   }
 };
 </script>
